2231a3027fd5e8ab4ad893228a788939d43d26b3
Colin Anderson <andercol.4@gmail.com>
2018-03-24 20:07:14 -0600

    added dependent destroy to campaigns and chars

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,7 +2,7 @@ class Campaign < ApplicationRecord
 
   belongs_to :game_master, foreign_key: 'game_master_id', class_name: 'User'
 
-  has_many :characters
+  has_many :characters, dependent: :destroy
 
   validates :name,
     uniqueness: {scope: :game_master_id},
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,4 @@
 class Character < ApplicationRecord
   belongs_to :campaign
+  has_many :responsibilities, dependent: :destroy
 end
